42ec9ad1ea91c3222d0f09758765ca7e23b106ec
Shaikot Ahmed <shaikot@BITMASCOT.local>
2019-10-30 15:39:38 +0600

    coomit msg

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Header from './components/Header';
 import Footer from './components/Footer';
 
+
 export default class About extends Component {
     render() {
         return (
